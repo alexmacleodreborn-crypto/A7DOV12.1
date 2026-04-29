e94f0966011858dfeca6f98beee4b8b854309b10
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-04-29 21:17:46 +0100

    Update registry.py

--- chassis/registry.py
+++ chassis/registry.py
@@ -1,6 +1,8 @@
 # chassis/registry.py
 
-def create_base_skeleton():
+from .growth import early_growth, mid_growth, late_growth
+
+def create_skeleton_registry():
     return [
         {
             "id": "spine_base",
@@ -8,33 +10,50 @@ def create_base_skeleton():
             "parent": None,
             "start": (0, 0, 0),
             "end": (0, 10, 0),
-        },
-        {
-            "id": "spine_mid",
-            "name": "Spine Mid",
-            "parent": "spine_base",
-            "start": (0, 10, 0),
-            "end": (0, 20, 0),
+            "dev": {
+                "t_start": 5,
+                "t_control": 80,
+                "t_load": 120,
+                "growth_curve": early_growth
+            }
         },
         {
             "id": "spine_top",
             "name": "Spine Top",
-            "parent": "spine_mid",
-            "start": (0, 20, 0),
-            "end": (0, 30, 0),
+            "parent": "spine_base",
+            "start": (0, 10, 0),
+            "end": (0, 25, 0),
+            "dev": {
+                "t_start": 10,
+                "t_control": 90,
+                "t_load": 130,
+                "growth_curve": mid_growth
+            }
         },
         {
             "id": "left_arm",
             "name": "Left Arm",
             "parent": "spine_top",
-            "start": (0, 25, 0),
-            "end": (-15, 20, 0),
+            "start": (0, 20, 0),
+            "end": (-15, 15, 0),
+            "dev": {
+                "t_start": 30,
+                "t_control": 110,
+                "t_load": 150,
+                "growth_curve": late_growth
+            }
         },
         {
             "id": "right_arm",
             "name": "Right Arm",
             "parent": "spine_top",
-            "start": (0, 25, 0),
-            "end": (15, 20, 0),
-        },
+            "start": (0, 20, 0),
+            "end": (15, 15, 0),
+            "dev": {
+                "t_start": 30,
+                "t_control": 110,
+                "t_load": 150,
+                "growth_curve": late_growth
+            }
+        }
     ]
